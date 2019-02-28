5d86142e3e49f5c9a77b70be58576a787cd521d2
kim.biloni <kim.biloni@he-arc.ch>
2019-02-28 20:17:10 +0100

    add synaiapp and remove the default one

--- Synai/urls.py
+++ Synai/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from dashboard import views
+from synaiapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ synaiapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SynaiappConfig(AppConfig):
+    name = 'synaiapp'
