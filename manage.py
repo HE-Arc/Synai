6b76cf8756265e735eb05cd134502c2697bcd45d
kim.biloni <kim.biloni@he-arc.ch>
2019-03-02 18:27:47 +0100

    use dotenv instead of the function in manage.py

--- manage.py
+++ manage.py
@@ -1,32 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-import re
-
-def read_env():
-    """Pulled from Honcho code with minor updates, reads local default
-    environment variables from a .env file located in the project root
-    directory.
-
-    source: https://gist.github.com/bennylope/2999704
-    """
-    try:
-        with open('.env') as f:
-            content = f.read()
-    except IOError:
-        content = ''
-
-    for line in content.splitlines():
-        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
-        if m1:
-            key, val = m1.group(1), m1.group(2)
-            m2 = re.match(r"\A'(.*)'\Z", val)
-            if m2:
-                val = m2.group(1)
-            m3 = re.match(r'\A"(.*)"\Z', val)
-            if m3:
-                val = re.sub(r'\\(.)', r'\1', m3.group(1))
-            os.environ.setdefault(key, val)
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Synai.settings')
@@ -38,5 +12,5 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    read_env()
+
     execute_from_command_line(sys.argv)
