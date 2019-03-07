cfbc2547eb45473ef4c73c1ae88ad83f01b777c9
Naralas <ludovic.herbelin@gmail.com>
2019-03-07 17:04:42 +0100

    Deployment config

--- Synai/settings.py
+++ Synai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(^*8t9(4w0a^d&ummb@^lek2w$gt*1-zsoz%m^-228ii-&jtd7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['synai.srvz-webapp.he-arc.ch']
 
 
 # Application definition
