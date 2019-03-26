18af71ecabf8cc42e4ecc6ea4e29389895db2dec
Naralas <ludovic.herbelin@gmail.com>
2019-03-26 10:04:11 +0100

    Forgot to import static

--- Synai/urls.py
+++ Synai/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 from synaiapp import views
 
